7b69f564bb900f6b488ec4f64a8f3fbcff0afa8a
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-27 14:05:05 -0300

    feat: added backend initial project

---
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/AtsresumegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.atsresumegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsresumegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsresumegeneratorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yannfigueiredo/atsresumegenerator/AtsresumegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.atsresumegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsresumegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
