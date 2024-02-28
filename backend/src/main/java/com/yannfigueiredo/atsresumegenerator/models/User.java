79fd57222def94615bf3b13aec3ab8e5127b38a2
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-27 21:04:47 -0300

    feat: added user model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/User.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/User.java
@@ -1,2 +1,43 @@
-package com.yannfigueiredo.atsresumegenerator.models;public class User {
+package com.yannfigueiredo.atsresumegenerator.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, length = 20, nullable = false)
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    @NotBlank
+    @Email
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank
+    @Size(min = 8, max = 30)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
