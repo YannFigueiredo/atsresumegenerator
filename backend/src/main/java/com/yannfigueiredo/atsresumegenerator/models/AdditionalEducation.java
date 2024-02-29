cd058ada5affbb41ff8e093febe4e5d10f6f6c53
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-28 21:10:33 -0300

    feat: added 'additional education' model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/AdditionalEducation.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/AdditionalEducation.java
@@ -1,2 +1,42 @@
-package com.yannfigueiredo.atsresumegenerator.models;public class AditionalEducation {
+package com.yannfigueiredo.atsresumegenerator.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.NumberFormat;
+
+@Entity
+@Table(name = "additional_education")
+public class AdditionalEducation {
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "course", nullable = false, length = 300)
+    @Size(min = 1, max = 300)
+    @NotBlank
+    private String course;
+
+    @Column(name = "institution", nullable = false, length = 300)
+    @Size(min = 1, max = 300)
+    @NotBlank
+    private String institution;
+
+    @Column(name = "workload", nullable = false, length = 20)
+    @Size(min = 1, max = 20)
+    @NotBlank
+    @NumberFormat(style = NumberFormat.Style.NUMBER)
+    private Float workload;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id", nullable = false, updatable = false)
+    private Resume resume;
 }
