e84cac95b92af850d89befaac1bf25be62f7ffdf
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-28 21:08:19 -0300

    feat: added 'other link' model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/OtherLink.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/OtherLink.java
@@ -1,2 +1,29 @@
-package com.yannfigueiredo.atsresumegenerator.models;public class OtherLink {
+package com.yannfigueiredo.atsresumegenerator.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Table(name = "other_link")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OtherLink {
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "link", nullable = false, length = 200)
+    @NotBlank
+    @URL
+    private String link;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id", nullable = false, updatable = false)
+    private Resume resume;
 }
