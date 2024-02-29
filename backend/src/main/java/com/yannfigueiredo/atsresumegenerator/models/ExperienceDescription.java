ff37dc60a60378e6fb981b5a45c2c880905fb382
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-28 21:09:02 -0300

    feat: added 'experience description' model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/ExperienceDescription.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/ExperienceDescription.java
@@ -1,12 +1,25 @@
 package com.yannfigueiredo.atsresumegenerator.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "education_description")
-public class EducationDescription {
+@Table(name = "experience_description")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ExperienceDescription {
     @Id
     @Column(name = "id", unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +31,6 @@ public class EducationDescription {
     private String description;
 
     @ManyToOne
-    @JoinColumn(name = "education_id", nullable = false)
-    private Education education;
+    @JoinColumn(name = "experience_id", nullable = false)
+    private Experience experience;
 }
