e479ca82550118623da6e7c31dd296efef5040c7
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-02-28 21:10:04 -0300

    feat: added 'additional info' model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/AdditionalInfo.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/AdditionalInfo.java
@@ -1,2 +1,29 @@
-package com.yannfigueiredo.atsresumegenerator.models;public class AdditionalInfo {
+package com.yannfigueiredo.atsresumegenerator.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "additional_info")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AdditionalInfo {
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "info", nullable = false, length = 500)
+    @Size(min = 1, max = 500)
+    @NotBlank
+    private String info;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id", nullable = false, updatable = false)
+    private Resume resume;
 }
