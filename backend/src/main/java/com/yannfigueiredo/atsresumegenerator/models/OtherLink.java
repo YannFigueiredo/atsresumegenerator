0268db1137043197be5f6318767ddf86e22c836e
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-05 19:47:49 -0300

    refactor: added serial in 'other link' model

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/OtherLink.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/models/OtherLink.java
@@ -7,12 +7,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "other_link")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class OtherLink {
+public class OtherLink implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @Column(name = "id", unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
