e9cf9e0b2fbc3230bbcfd8d46e573c6584221db7
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-05 19:51:04 -0300

    feat: added factory to create mock

--- backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
+++ backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
@@ -1,2 +1,16 @@
-package com.yannfigueiredo.atsresumegenerator;public class Factory {
+package com.yannfigueiredo.atsresumegenerator;
+
+import com.yannfigueiredo.atsresumegenerator.models.Resume;
+import com.yannfigueiredo.atsresumegenerator.models.User;
+
+public class Factory {
+    public static User createUser(String name) {
+        return new User(
+                1L,
+                name,
+                "yannfabricio@gmail.com",
+                "senha123",
+                new Resume()
+        );
+    }
 }
