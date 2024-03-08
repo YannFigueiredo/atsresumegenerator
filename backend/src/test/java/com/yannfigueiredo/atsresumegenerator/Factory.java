fa7f690621dc9c4bafb36c311cb31d11f2978191
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-08 20:20:07 -0300

    refactor: added function for create a resume model

--- backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
+++ backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
@@ -1,16 +1,28 @@
 package com.yannfigueiredo.atsresumegenerator;
 
-import com.yannfigueiredo.atsresumegenerator.models.Resume;
-import com.yannfigueiredo.atsresumegenerator.models.User;
+import com.yannfigueiredo.atsresumegenerator.models.*;
 
 public class Factory {
-    public static User createUser(String name) {
-        return new User(
-                1L,
-                name,
-                "yannfabricio@gmail.com",
-                "senha123",
-                new Resume()
-        );
+    public static User createUser(String username) {
+        User user = new User();
+
+        user.setUsername(username);
+        user.setEmail("yannfabricio@gmail.com");
+        user.setPassword("senha123");
+
+        return user;
+    }
+
+    public static Resume createResume(String name) {
+        Resume resume = new Resume();
+
+        resume.setName(name);
+        resume.setTelephoneNumber(1112345678);
+        resume.setEmail("yannfabricio@gmail.com");
+        resume.setLinkedin("https://linkedin.com/in/yannfigueiredo");
+        resume.setObjective("Desenvolvedor Java");
+        resume.setProfessionalSummary("Resumo profissional");
+
+        return resume;
     }
 }
