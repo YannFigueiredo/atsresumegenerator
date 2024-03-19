3b020b7bfdda48753a847fdc04e84bf69b447246
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-19 09:03:34 -0300

    feat: added 'find all' operation

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/ExperienceRepository.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/ExperienceRepository.java
@@ -3,5 +3,8 @@ package com.yannfigueiredo.atsresumegenerator.repositories;
 import com.yannfigueiredo.atsresumegenerator.models.Experience;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+    List<Experience> findByResumeId(Long resumeId);
 }
