237b07509366c2479663c59136a743044e8e1d8a
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-19 09:01:57 -0300

    feat: added create functions in factory

--- backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
+++ backend/src/test/java/com/yannfigueiredo/atsresumegenerator/Factory.java
@@ -2,6 +2,8 @@ package com.yannfigueiredo.atsresumegenerator;
 
 import com.yannfigueiredo.atsresumegenerator.models.*;
 
+import java.time.Instant;
+
 public class Factory {
     public static User createUser(String username) {
         User user = new User();
@@ -24,5 +26,24 @@ public class Factory {
         resume.setProfessionalSummary("Resumo profissional");
 
         return resume;
+    }
+
+    public static OtherLink createOtherLink(String link) {
+        OtherLink otherLink = new OtherLink();
+
+        otherLink.setLink(link);
+
+        return otherLink;
+    }
+
+    public static Experience createExperience(String jobTitle) {
+        Experience experience = new Experience();
+
+        experience.setExpBegin(Instant.now());
+        experience.setExpEnd(Instant.now());
+        experience.setJobTitle(jobTitle);
+        experience.setEnterpriseName("Microsoft");
+
+        return experience;
     }
 }
