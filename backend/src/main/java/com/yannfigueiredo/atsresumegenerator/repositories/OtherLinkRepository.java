7194d02a20049eef1349e17e6f320b97fa220031
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-19 09:04:09 -0300

    feat: added 'find all' operation

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/OtherLinkRepository.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/OtherLinkRepository.java
@@ -3,5 +3,8 @@ package com.yannfigueiredo.atsresumegenerator.repositories;
 import com.yannfigueiredo.atsresumegenerator.models.OtherLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OtherLinkRepository extends JpaRepository<OtherLink, Long> {
+    List<OtherLink> findByResumeId(Long resumeId);
 }
