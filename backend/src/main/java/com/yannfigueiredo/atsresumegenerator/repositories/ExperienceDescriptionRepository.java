1ff70198127d20199794ea6f1086239c2d2cf123
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-19 21:48:51 -0300

    feat: added 'find all by experiend id' operation

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/ExperienceDescriptionRepository.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/repositories/ExperienceDescriptionRepository.java
@@ -3,5 +3,8 @@ package com.yannfigueiredo.atsresumegenerator.repositories;
 import com.yannfigueiredo.atsresumegenerator.models.ExperienceDescription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExperienceDescriptionRepository extends JpaRepository<ExperienceDescription, Long> {
+    List<ExperienceDescription> findByExperienceId(Long descriptionId);
 }
