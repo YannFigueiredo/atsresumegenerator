4b9fe3cc1a24b3255a9171ebf20c42b06e3dd13d
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-03-29 18:49:54 -0300

    feat: added resume controller

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/controllers/ResumeController.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/controllers/ResumeController.java
@@ -1,2 +1,49 @@
-package com.yannfigueiredo.atsresumegenerator.controllers;public class ResumeController {
+package com.yannfigueiredo.atsresumegenerator.controllers;
+
+import com.yannfigueiredo.atsresumegenerator.models.Resume;
+import com.yannfigueiredo.atsresumegenerator.services.ResumeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/resume")
+public class ResumeController {
+    @Autowired
+    ResumeService resumeService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Resume> findById(@PathVariable Long id) {
+        Resume obj = this.resumeService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Resume> create(@Valid @RequestBody Resume obj) {
+        Resume resume = this.resumeService.create(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(resume.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(resume);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Resume> update(@Valid @RequestBody Resume obj, @PathVariable Long id) {
+        Resume resume = this.resumeService.update(id, obj);
+
+        return ResponseEntity.ok().body(resume);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.resumeService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
