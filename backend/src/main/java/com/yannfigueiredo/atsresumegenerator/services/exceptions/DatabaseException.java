49013fe59fcfc94976f815e911217759c4fe8f24
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-04-05 11:04:12 -0300

    feat: added 'database error' personalized exception

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/services/exceptions/DatabaseException.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/services/exceptions/DatabaseException.java
@@ -1,2 +1,9 @@
-package com.yannfigueiredo.atsresumegenerator.services.exceptions;public class DatabaseException {
+package com.yannfigueiredo.atsresumegenerator.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
 }
