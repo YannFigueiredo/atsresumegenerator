fe85d9a26743bf753d0f4b769906342459bc151b
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-04-05 11:04:45 -0300

    feat: added standard error object

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/controllers/exceptions/StandardError.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/controllers/exceptions/StandardError.java
@@ -1,2 +1,18 @@
-package com.yannfigueiredo.atsresumegenerator.controllers.exceptions;public class StandardError {
+package com.yannfigueiredo.atsresumegenerator.controllers.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
 }
