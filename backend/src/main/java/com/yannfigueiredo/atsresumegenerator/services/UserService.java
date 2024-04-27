c450c9e2ed8751aaa9b1b133c87ebcb63d46239e
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-04-27 11:06:40 -0300

    refactor: adjusted user service

--- backend/src/main/java/com/yannfigueiredo/atsresumegenerator/services/UserService.java
+++ backend/src/main/java/com/yannfigueiredo/atsresumegenerator/services/UserService.java
@@ -2,7 +2,11 @@ package com.yannfigueiredo.atsresumegenerator.services;
 
 import com.yannfigueiredo.atsresumegenerator.models.User;
 import com.yannfigueiredo.atsresumegenerator.repositories.UserRepository;
+import com.yannfigueiredo.atsresumegenerator.services.exceptions.DatabaseException;
+import com.yannfigueiredo.atsresumegenerator.services.exceptions.ObjectNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +20,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
 
-        return user.orElseThrow(() -> new RuntimeException(
+        return user.orElseThrow(() -> new ObjectNotFoundException(
                 "Usuário não encontrado!"
         ));
     }
@@ -36,16 +40,20 @@ public class UserService {
             newUser.setPassword(user.getPassword() != null ? user.getPassword() : newUser.getPassword());
 
             return this.userRepository.save(newUser);
-        } catch(Exception e) {
-            throw new RuntimeException("Não foi possível atualizar o usuário! Erro: " + e.getMessage());
+        } catch(EntityNotFoundException e) {
+            throw new ObjectNotFoundException("Não foi possível atualizar o usuário! Erro: " + e.getMessage());
         }
     }
 
     public void delete(Long id) {
         try {
             this.userRepository.deleteById(id);
-        } catch(Exception e) {
-            throw new RuntimeException("Não foi possível deletar o usuário! Erro: " + e.getMessage());
+        }
+        catch(EntityNotFoundException e) {
+            throw new ObjectNotFoundException("Usuário não encontrado!");
+        }
+        catch(DataIntegrityViolationException e) {
+            throw new DatabaseException("Não foi possível deletar o usuário! Erro: " + e.getMessage());
         }
     }
 }
